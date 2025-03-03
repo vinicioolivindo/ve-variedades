d1690f92f26487e796bbaf5649c9472c692c16bf
Vinicio :) <161966620+vinicioolivindo@users.noreply.github.com>
2025-03-03 16:52:53 -0300

    .

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,4 +1,4 @@
-import { ChevronLeftIcon } from "lucide-react";
+import { ChevronLeftIcon, House } from "lucide-react";
 import { useNavigate, useParams } from "react-router-dom";
 import products from "../data/products.json";
 
@@ -33,17 +33,18 @@ const Product = () => {
         <div className="flex justify-center relative">
             <button onClick={() => navigate(-1)} className="flex absolute left-2 top-3">
                 <ChevronLeftIcon />
-                Menu
+                <p className="md:block hidden">Menu</p>
+                <p className="md:hidden block"><House/></p>
             </button>
-            <div className="flex flex-col items-center max-w-6xl mt-4">
+            <div className="flex flex-col items-center max-w-6xl mt-6">
                 <h1 className="text-2xl">{produto.nameProd}</h1>
                 <ul className="flex flex-wrap justify-center pt-3 gap-3">
                     {produto.items.map((prod) => (
                         <li key={prod.valor}>
-                            <div className="w-72 h-56">
-                                <img className="w-full h-full object-cover " src={prod.imagem} alt="" />
+                            <div className="w-72 h-56 rounded-t-lg overflow-hidden">
+                                <img className="w-full h-full object-cover" src={prod.imagem} alt="" />
                             </div>
-                            <div className="flex justify-center relative">
+                            <div className="flex justify-center relative bg-primaryColor rounded-b-lg">
                                 <h2 className="absolute left-2">{prod.tamanho}</h2>
                                 <h3 className="text-lg">R${prod.valor},00</h3>
                             </div>
