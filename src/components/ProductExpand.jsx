68ac7f99db2634d8e3ce7c0cea39f1bda96e47a0
Vinicio :) <161966620+vinicioolivindo@users.noreply.github.com>
2025-03-03 18:17:12 -0300

    api wats implementada

--- src/components/ProductExpand.jsx
+++ src/components/ProductExpand.jsx
@@ -1,17 +1,39 @@
+/* eslint-disable react/prop-types */
+import { X } from "lucide-react";
 import Button from "./Button";
+import { WhatsappLogo } from "@phosphor-icons/react";
 
 // eslint-disable-next-line react/prop-types
-const ProductExpand = ({image, tam}) => {
+const ProductExpand = ({ isOpen, onClose, prod }) => {
+    if (!isOpen || !prod) return null;
+
     return (
-        <div className="max-w-sm bg-silver p-4 rounded-xl text-D_Gray m-4 mb-10">
-          <img className="w-32 h-32"
-                  src={image}
-                  alt=""/>
-    
-        <p>{tam}</p>
-          <Button color={"bg-primaryColor"}>Entendido</Button>
+        <div
+            className="fixed inset-0 bg-black_10 bg-opacity-85 flex items-center justify-center z-50 -top-6"
+            onClick={onClose}
+        >
+            <div
+                className="relative p-4 max-w-md mx-auto"
+                onClick={(e) => e.stopPropagation()} // Impede o fechamento ao clicar dentro do modal
+            >
+                <button
+                    className="absolute z-20 right-5 top-5 rounded-md bg-primaryColor text-white "
+                    onClick={onClose}
+                >
+                    <X size={30} />
+                </button>
+                <img
+                    src={prod.imagem}
+                    className="w-full rounded-lg mb-5"
+                />
+
+                <Button color={"bg-secondaryColor"} link={`https://wa.me/5588994859101?text=Ol%C3%A1%2C%20fiquei%20interessada(o)%20nesse%20produto! %20%0AModelo: ${prod.imagem} %0ATamanho: ${prod.tamanho}%20%0AValor: ${prod.valor},00R$`}>
+                    <WhatsappLogo size={22}/>
+                    Comprar
+                </Button>
+            </div>
         </div>
-      )
+    );
 }
 
 export default ProductExpand;
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,10 +1,24 @@
 import { ChevronLeftIcon, House } from "lucide-react";
 import { useNavigate, useParams } from "react-router-dom";
 import products from "../data/products.json";
+import ProductExpand from "../components/ProductExpand";
+import { useState } from "react";
 
 const Product = () => {
     const { categoria, tituloId } = useParams();
     const navigate = useNavigate();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedProd, setselectedProd] = useState({});
+
+    const openModal = (prod) => {
+        setselectedProd(prod);
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+        setselectedProd(null);
+    };
 
     // Encontre a categoria correspondente no JSON
     const categoriaEncontrada = products.categories.find(
@@ -28,30 +42,32 @@ const Product = () => {
         return null;
     }
 
-
     return (
         <div className="flex justify-center relative">
+
             <button onClick={() => navigate(-1)} className="flex absolute left-2 top-3">
                 <ChevronLeftIcon />
                 <p className="md:block hidden">Menu</p>
-                <p className="md:hidden block"><House/></p>
+                <p className="md:hidden block"><House /></p>
             </button>
             <div className="flex flex-col items-center max-w-6xl mt-6">
                 <h1 className="text-2xl">{produto.nameProd}</h1>
                 <ul className="flex flex-wrap justify-center pt-3 gap-3">
                     {produto.items.map((prod) => (
                         <li key={prod.valor}>
-                            <div className="w-72 h-56 rounded-t-lg overflow-hidden">
+                            <div className="w-72 h-56 rounded-t-lg overflow-hidden" onClick={()=> openModal(prod)}
+                            >
                                 <img className="w-full h-full object-cover" src={prod.imagem} alt="" />
                             </div>
-                            <div className="flex justify-center relative bg-primaryColor rounded-b-lg">
-                                <h2 className="absolute left-2">{prod.tamanho}</h2>
-                                <h3 className="text-lg">R${prod.valor},00</h3>
+                            <div className="flex justify-center items-center relative bg-primaryColor rounded-b-lg py-1">
+                                <h2 className="absolute text-lg left-4 text-white font-semibold">{prod.tamanho}</h2>
+                                <h3 className="text-lg text-white font-semibold">R${prod.valor},00</h3>
                             </div>
                         </li>
                     ))}
                 </ul>
             </div>
+            <ProductExpand isOpen={isModalOpen} onClose={closeModal} prod={selectedProd}/>
         </div>
     );
 };
